9cb50f231f85f687c7468f72aa0ed99a0ea1324c
junjujun <ftwlpsl@gmail.com>
2024-12-03 23:51:12 +0900

    int to BigDecimal in GiveAllowanceUseCaseTest class.

--- app/src/test/java/org/example/GiveAllowanceUseCaseTest.java
+++ app/src/test/java/org/example/GiveAllowanceUseCaseTest.java
@@ -2,22 +2,23 @@ package org.example;
 
 import org.junit.jupiter.api.Test;
 
+import java.math.BigDecimal;
+
 import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertTrue;
 
 public class GiveAllowanceUseCaseTest {
     @Test
     public void testGiveAllowance() {
         final Child taro = new Child("Taro");
-        final Money fiveHundred = new Money(500);
+        final Money fiveHundred = new Money(BigDecimal.valueOf(500));
         taro.receiveAllowance(fiveHundred);
         assertEquals(fiveHundred, taro.pocket());
 
         final Child jiro = new Child("Jiro");
-        final Money oneThousand = fiveHundred.plus(500);
+        final Money oneThousand = fiveHundred.plus(BigDecimal.valueOf(500));
         jiro.receiveAllowance(oneThousand);
         assertEquals(oneThousand, jiro.pocket());
 
-        assertEquals(new Money(500), taro.pocket());
+        assertEquals(new Money(BigDecimal.valueOf(500)), taro.pocket());
     }
 }
